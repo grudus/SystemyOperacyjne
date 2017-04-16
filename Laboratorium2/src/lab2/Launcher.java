9a1c5d4e940a498b229889e7174739e21b6f8d98
grudus <jakgruda@gmail.com>
2017-04-16 21:19:28 +0200

    Change packages and modules

--- Laboratorium2/src/lab2/Launcher.java
+++ Laboratorium2/src/lab2/Launcher.java
@@ -7,30 +7,34 @@ import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static java.util.stream.IntStream.range;
 
 public class Launcher {
     public static void main(String[] args) {
         final Random RANDOM = new Random();
 
-        final int DISK_SIZE = /*RANDOM.nextInt(1000);*/4999;
-        final int HEADER_POSITION = /*RANDOM.nextInt(DISK_SIZE);*/ 143;
-        final int PREVIOUS_POSITION = /*RANDOM.nextInt(DISK_SIZE);*/ 125;
+        final int DISK_SIZE = RANDOM.nextInt(1000);
+        final int HEADER_POSITION = RANDOM.nextInt(DISK_SIZE);
+        final int PREVIOUS_POSITION = RANDOM.nextInt(DISK_SIZE);;
+
+
+
 
         List<DiskRequest> requestList =
-//                range(0, 10).mapToObj(i -> DiskRequest.random(DISK_SIZE, 1, DISK_SIZE / 5))
-////                .peek(System.out::println)
-//                .collect(Collectors.toList())
+                range(0, 10).mapToObj(i -> DiskRequest.random(DISK_SIZE, 1, DISK_SIZE / 5))
+//                .peek(System.out::println)
+                .collect(Collectors.toList())
 
-                asList(new DiskRequest(86),
-                        new DiskRequest(1470),
-                        new DiskRequest(913),
-                        new DiskRequest(1774),
-                        new DiskRequest(948),
-                        new DiskRequest(1509),
-                        new DiskRequest(1022),
-                        new DiskRequest(1750),
-                        new DiskRequest(130)
-                );
+//                asList(new DiskRequest(86),
+//                        new DiskRequest(1470),
+//                        new DiskRequest(913),
+//                        new DiskRequest(1774),
+//                        new DiskRequest(948),
+//                        new DiskRequest(1509),
+//                        new DiskRequest(1022),
+//                        new DiskRequest(1750),
+//                        new DiskRequest(130)
+//                );
         ;
 
         List<Algorithm<DiskRequest>> algorithms = asList(new FcfsAlgorithm(DISK_SIZE, HEADER_POSITION),
@@ -39,6 +43,7 @@ public class Launcher {
                 new CScanAlgorithm(DISK_SIZE, HEADER_POSITION, PREVIOUS_POSITION),
                 new EdfAlgorithm(DISK_SIZE, HEADER_POSITION));
 
+        System.out.println(format("Disk size is %d and header position %d", DISK_SIZE, HEADER_POSITION));
         algorithms.forEach(algorithm -> {
             algorithm.process(copy(requestList));
             System.out.println(format("%s, distance: %d", algorithm, algorithm.getDistance()));
--- Laboratorium2/src/lab2/ScanAlgorithm.java
+++ Laboratorium2/src/lab2/ScanAlgorithm.java
@@ -2,7 +2,6 @@ package lab2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static java.lang.Math.abs;
 import static java.util.Comparator.comparingInt;
